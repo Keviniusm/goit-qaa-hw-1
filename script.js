1aec64eaa0dd8456e5aec76dc00906029bdd2607
Keviniusm <158042994+Keviniusm@users.noreply.github.com>
2024-03-07 09:59:26 +0100

    Zadanie domowe 1 (nie wiem, czy musiałem wszystko wrzucać, czy wystarczył index.html)

---
+++ script.js
@@ -0,0 +1,20 @@
+// funkcja do zmiany naglowka
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "Nowy nagłówek";
+}
+
+// funkcja do zmiany koloru         
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "red";
+}
+
+//przykladowy komentarz
+
+/*
+przykladowy
+komentarz
+*/
+
+console.log('jaks tam tekst');
